59c4a75b2498a35f49b0483ab474e0ba74f6b557
Sofía Campos <133938038+soficam@users.noreply.github.com>
2025-09-23 18:15:29 -0600

    Commit Proyecto Semana 2

---
+++ TiendaTech/src/main/java/tiendaTech/com/demo/TiendaTechApplication.java
@@ -0,0 +1,13 @@
+package tiendaTech.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTechApplication.class, args);
+	}
+
+}
---
+++ TiendaTech/src/test/java/tiendaTech/com/demo/TiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package tiendaTech.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
